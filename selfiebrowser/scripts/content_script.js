c6c671044fef801253d2d5a97ed8516010301ea6
Lauren McCarthy <laurmccarthy@gmail.com>
2015-06-12 17:06:38 -0400

    progress

---
+++ selfiebrowser/scripts/content_script.js
@@ -0,0 +1,36 @@
+
+$("document").ready(function(){
+    
+
+  navigator.getUserMedia  = navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia;
+
+  var video = document.createElement('video');
+  var video_container = document.createElement('div');
+  $(video_container).append(video);
+  $('body').append(video_container);
+
+  $(video).css('width', '240px');
+  $(video).css('height', '180px');
+  $(video).css('margin-top', '-15px');
+  $(video).css('margin-left', '-45px');
+  
+  $(video_container).css('position', 'fixed');
+  $(video_container).css('bottom', '20px');
+  $(video_container).css('left', '20px');
+  $(video_container).css('width', '150px');
+  $(video_container).css('height', '150px');
+  $(video_container).css('border', '3px solid rgb(0, 216, 255)');
+  $(video_container).css('overflow', 'hidden');
+  $(video_container).css('z-index', '4');
+
+  navigator.getUserMedia({audio: false, video: true}, function(stream) {
+    video.src = window.URL.createObjectURL(stream);
+    video.play();
+  }, function(error) {
+    console.log(error);
+  });
+
+});
---
+++ upload.sh
@@ -0,0 +1,8 @@
+youtube-upload \
+  --client-secrets=api_credentials.json \
+  --privacy=public \
+  --description="testing" \
+  --title="eo testing" \
+  --title-template="{title} ({n}/{total})" \
+  --location="latitude=40.687730, longitude=-73.979779, altitude=0" \
+  /Users/lmccart/Desktop/eo_recordings/MyStream_0.mov
