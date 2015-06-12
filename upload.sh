a3cc84424b149d025d6a521401ad5c47c0b402a8
Lauren McCarthy <laurmccarthy@gmail.com>
2015-06-12 19:48:34 -0400

    scp

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
 /usr/local/bin/youtube-upload \
   --client-secrets=/Users/lmccart/Documents/eo/api_credentials.json \
   --privacy=public \
-  --description="testing" \
-  --title="eo testing" \
+  --description="electric objects five dollar commission lmccart" \
+  --title=$2 \
   --title-template="{title} ({n}/{total})" \
   --location="latitude=40.687730, longitude=-73.979779, altitude=0" \
   $1
