7e4f1b997e0e9013cef2a0b48e06801cd268d6aa
Lauren McCarthy <laurmccarthy@gmail.com>
2015-06-12 17:45:22 -0400

    added yt upload

--- upload.sh
+++ upload.sh
@@ -1,8 +1,8 @@
-youtube-upload \
-  --client-secrets=api_credentials.json \
+/usr/local/bin/youtube-upload \
+  --client-secrets=/Users/lmccart/Documents/eo/api_credentials.json \
   --privacy=public \
   --description="testing" \
   --title="eo testing" \
   --title-template="{title} ({n}/{total})" \
   --location="latitude=40.687730, longitude=-73.979779, altitude=0" \
-  /Users/lmccart/Desktop/eo_recordings/MyStream_0.mov
+  $1
